04524b57107c50f7d1b583c2295e206c3ef4ce5f
Binara Heshan Wasala <bhwasala03@gmail.com>
2026-03-25 13:43:07 +0530

    Refactor db.js for improved readability

--- db.js
+++ db.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: 'postgres',
     logging: false
 });
+//sequlize
 
 sequelize.authenticate()
     .then(() => console.log('Database connected...'))
