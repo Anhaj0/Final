4226b5b035abec9f9549a4bc4a396cd59b8b83d8
Binara Heshan Wasala <bhwasala03@gmail.com>
2026-03-25 13:21:35 +0530

    adding

---
+++ lost .js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const User = require('./User');
+
+const LostFound = sequelize.define('LostFound', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    itemName: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT },
+    status: { type: DataTypes.ENUM('lost','found','returned'), defaultValue: 'lost' }
+});
+
+LostFound.belongsTo(User);
+
+module.exports = LostFound;
---
+++ rewrad.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+const User = require('./User');
+
+const Reward = sequelize.define('Reward', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    points: { type: DataTypes.INTEGER, defaultValue: 0 }
+});
+
+Reward.belongsTo(User);
+
+module.exports = Reward;
