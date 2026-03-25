8c15187ac6f8abf6851ec3c51ed93829aeb8a4e1
Binara Heshan Wasala <bhwasala03@gmail.com>
2026-03-25 13:20:51 +0530

    adding

---
+++ buscontoller.js
@@ -0,0 +1,20 @@
+const Bus = require('../models/Bus');
+
+exports.updateLocation = async (req, res) => {
+    const { busId, lat, lng } = req.body;
+    try {
+        const bus = await Bus.findByPk(busId);
+        if (!bus) return res.status(404).json({ message: 'Bus not found' });
+
+        bus.location = { lat, lng };
+        await bus.save();
+        res.json({ message: 'Location updated', bus });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getAllBuses = async (req, res) => {
+    const buses = await Bus.findAll();
+    res.json(buses);
+};
---
+++ rewardcontroler.js
@@ -0,0 +1,15 @@
+const Reward = require('../models/Reward');
+
+exports.getUserPoints = async (req, res) => {
+    const { userId } = req.params;
+    const reward = await Reward.findOne({ where: { UserId: userId } });
+    res.json(reward || { points: 0 });
+};
+
+exports.addPoints = async (req, res) => {
+    const { userId, points } = req.body;
+    let reward = await Reward.findOne({ where: { UserId: userId } });
+    if (!reward) reward = await Reward.create({ UserId: userId, points });
+    else reward.points += points, await reward.save();
+    res.json(reward);
+};
